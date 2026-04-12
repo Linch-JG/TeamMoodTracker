a8e9e72de935c0a93630e322dcf8b2e5075500a1
Vladimir Zhidkov <v.zhidkov@innopolis.university>
2026-04-13 00:48:34 +0300

    Good CI test

--- src/team_mood_tracker/backend/bad_demo.py
+++ src/team_mood_tracker/backend/bad_demo.py
@@ -1,7 +1,10 @@
-x=1+2
-y=x*3
-def calculate(a,b,c,d,e,f):
+x = 1 + 2
+y = x * 3
+
+
+def calculate(a, b, c, d, e, f):
+    """Calculate the sum of six values."""
     if a:
-     if b:
-      if c:
-       return a+b+c+d+e+f
+        if b:
+            if c:
+                return a + b + c + d + e + f
